4d018d9a9eca5826baa6c0547ec144b2ef5934e7
Dihan <rafeeuzzamandihan@gmail.com>
2025-09-07 17:03:12 +0600

    Add default layout and integrate it into the app structure

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
 </template>
+<script setup lang="ts">
+</script>
---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main class="mx-auto max-w-[1300px] lg:max-w-[1005px] xl:max-w-[1160px] min-[1300px]:max-w-[1256px] 2xl:max-w-[1300px] max-[1300px]:p-4">
+      <slot />
+    </main>
+</template>
+
+<style scoped>
+
+</style>
